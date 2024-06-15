e10b93c596da7a6eebddd9df30f67bf2261701d4
monofuel <monofuel@japura.net>
2024-06-15 16:14:43 -0400

    fixing up tests

---
+++ packages/contracts/test/TablesTest.t.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.24;
+
+import "forge-std/Test.sol";
+import { MudTest } from "@latticexyz/world/test/MudTest.t.sol";
+
+// Test file for various table helpers
+
+contract TableTest is MudTest {
+  function setUp() public override {
+  }
+  
+}
